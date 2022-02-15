d15df5b4542fa53b830c74a553a5d82544daddd9
bhavesh215 <bhaveshkumargci2018@gmail.com>
2022-02-15 15:19:45 +0530

    Learned strings and it's methods in JS

---
+++ Javascript/string_methods.js
@@ -0,0 +1,21 @@
+//Different Methods of Strings in JS
+
+// length Method
+let favShow='Shark Tank India';
+console.log(favShow.length);
+
+console.log(favShow[favShow.length-1]);
+
+// indexOf Method
+console.log(favShow.indexOf('Tank'));
+
+// slice Method
+console.log(favShow.slice(6,10));
+
+var DaraSingh='Mera NaAm DarA siNgh haI';
+
+// toUpperCase Method
+console.log(DaraSingh.toUpperCase());
+
+// toLowerCase Method
+console.log(DaraSingh.toLowerCase());
---
+++ Javascript/strings.js
@@ -0,0 +1,45 @@
+// What is string
+
+// String is a  js datatype which is used to store a piece of text 
+
+//How to define string 
+var name="Bhavesh Uttawani";
+
+//Some exapmpes of bad strings
+
+/*let bs1= Thisi is a string;
+let bs2= 'Thisi is a string;
+let bs3=  Thisi is a str';
+console.log(bs1,bs2,bs3); */
+
+//String Concatenation
+
+/* Example 1
+ let pname=prompt('Hey What is your name');
+ let greet = "Hello";
+ let welcome="Welcome to Grow Initiatives";
+ let outmsg= greet+" "+pname+" "+welcome;
+ alert(outmsg);
+
+*/
+
+//Concatenation of string with a number
+/* let string="Hello";
+ let number=123;
+ console.log(typeof (string+number));*/
+
+let a,b;
+
+a="124";
+b=24;
+
+//Convrsion of string to number
+
+let c=Number(a);
+
+console.log('c = ',++c);
+
+//Conversion of number to string
+let d= b.toString();
+
+console.log('d = ',d+0);
